c1609577274528f6931b966f17b38067289555c5
Faruk Ogunsola <ogunsolafaruk8@example.com>
2025-08-02 13:07:44 +0100

    footer

--- src/pages/contact/Contactpage.jsx
+++ src/pages/contact/Contactpage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../../ui/Header';
-import Page from './page';
+import Page from '../contact/Page';
 const Contactpage = () => {
     return (
         <div>
